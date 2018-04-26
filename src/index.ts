4693413bb1725b2d81ab0c36eb4923e7fe1a924b
DaniyarJakupov <ddr199305@gmail.com>
2018-04-26 15:02:29 +0200

    Add express to serve images

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Prisma } from './generated/prisma'
-import resolvers from './resolvers'
+import { GraphQLServer } from 'graphql-yoga';
+const express = require('express');
+
+import { Prisma } from './generated/prisma';
+import resolvers from './resolvers';
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -10,9 +12,11 @@ const server = new GraphQLServer({
     db: new Prisma({
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in .env)
       secret: process.env.PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
-      debug: true, // log all GraphQL queries & mutations
-    }),
-  }),
-})
+      debug: true // log all GraphQL queries & mutations
+    })
+  })
+});
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.express.use('/images', express.static('images'));
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
